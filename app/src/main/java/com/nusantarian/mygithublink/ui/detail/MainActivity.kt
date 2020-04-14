293976cbe85e5db30bb2473351c097fcb877de29
rogcomfox <joeg.11021996@gmail.com>
2020-04-14 15:08:27 +0700

    init commit

---
+++ app/src/main/java/com/nusantarian/mygithublink/ui/detail/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.nusantarian.mygithublink.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nusantarian.mygithublink.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/nusantarian/mygithublink/ui/splash/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.nusantarian.mygithublink.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.nusantarian.mygithublink.R
+import com.nusantarian.mygithublink.ui.detail.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, 2000)
+    }
+}
